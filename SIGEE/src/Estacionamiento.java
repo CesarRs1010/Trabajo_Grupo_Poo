d5bdaa39372f28f547e4c439e80f99db49f7f3b0
Alexis <Belito11alex@gmail.com>
2024-12-06 16:29:55 -0500

    se corrigio error

--- SIGEE/src/Estacionamiento.java
+++ SIGEE/src/Estacionamiento.java
@@ -1,7 +1,26 @@
-public class EstacionamientoMai {
-    public static void main(String[] args) {
-        DatosSimulacion datosSimulacion = new DatosSimulacion(10);  // Crear la simulación con 10 espacios
-        AplicacionMovil app = new AplicacionMovil();
-        app.iniciarAplicacion(datosSimulacion);
+public abstract class Estacionamiento {
+    private final String idEspacio;
+    protected boolean ocupado;
+
+    public Estacionamiento(String idEspacio) {
+        this.idEspacio = idEspacio;
+        this.ocupado = false;
+    }
+
+    public abstract void ocuparEspacio();
+    public abstract void liberarEspacio();
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public String getIdEspacio() {
+        return idEspacio;
+    }
+
+    // Método para obtener el tiempo desde la última liberación del espacio
+    public int obtenerTiempoDesdeUltimaLiberacion() {
+        System.out.println("Calculando el tiempo desde la última liberación para el espacio: " + idEspacio);
+        return 10; // Retorna un valor de ejemplo
     }
 }
--- SIGEE/src/EstacionamientoMai.java
+++ SIGEE/src/EstacionamientoMai.java
@@ -1,26 +1,7 @@
 public class EstacionamientoMai {
-    public static void main(String[] args) throws Exception {
-        SensorOcupacion sensor = new SensorOcupacion();
+    public static void main(String[] args) {
+        DatosSimulacion datosSimulacion = new DatosSimulacion(10);  // Crear la simulación con 10 espacios
         AplicacionMovil app = new AplicacionMovil();
-        EspacioMoto espacio1 = new EspacioMoto("M1");
-        ModuloAdministracion admin = new ModuloAdministracion(10);
-        SeñalizacionVisual señal = new SeñalizacionVisual();
-
-        // Probar la asignacion de espacio
-        sensor.detectarOcupacion();
-        app.asignarEspacio(espacio1);
-        admin.incrementarEspaciosOcupados();  // Incrementar espacios ocupados después de asignar
-        señal.mostrarEstado(espacio1);
-        señal.mostrarDetalleEstado(espacio1);
-
-        // Parte para la liberacion de espacio
-        sensor.detectarLibreracion();
-        espacio1.liberarEspacio();
-        admin.decrementarEspaciosOcupados();  // Decrementar espacios ocupados después de liberar
-        señal.mostrarEstado(espacio1);
-        señal.mostrarDetalleEstado(espacio1);
-
-        // Parte en la que se va a generar un reporte desde AplicacionMovil
-        app.mostrarReporteAplicacionMovil(espacio1, admin);
+        app.iniciarAplicacion(datosSimulacion);
     }
 }
