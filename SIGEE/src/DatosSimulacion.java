4533dbb71db7a60fbb133fccc168956695bc23a3
Andrey <andreyq68@hotmail.com>
2024-12-06 17:06:33 -0500

    se agregaron y actualizaron los metodos

--- SIGEE/src/DatosSimulacion.java
+++ SIGEE/src/DatosSimulacion.java
@@ -30,4 +30,3 @@ public class DatosSimulacion {
         admin.resetearEspacios();
     }
 }
-
--- SIGEE/src/SensorOcupacion.java
+++ SIGEE/src/SensorOcupacion.java
@@ -2,10 +2,14 @@ public class SensorOcupacion implements Sensor {
     @Override
     public void detectarOcupacion() {
         System.out.println("El sensor ha detectado que el espacio está ocupado.");
+        System.out.println("################ Sensor activado #################");
+        System.out.println("############## Color Rojo: OCUPADO ##############");
     }
 
     @Override
     public void detectarLibreracion() {
         System.out.println("El sensor ha detectado que el espacio está libre.");
+        System.out.println("################ Sensor activado #################");
+        System.out.println("############## Color Verde: Libre #################");
     }
 }
